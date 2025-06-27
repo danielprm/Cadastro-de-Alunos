4095e6a212547c4f2109e65c3debd4e65c6956cc
daniel <danielprmjob@gmail.com>
2025-06-26 23:02:54 -0300

    modificando algumas legendas.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Se você quiser começar a medir o desempenho no seu app, passe uma função
+// para registrar os resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint de analytics. Saiba mais: https://bit.ly/CRA-vitals
 reportWebVitals();
